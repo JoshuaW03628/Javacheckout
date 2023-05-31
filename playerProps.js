73d3aabc7440cc63265f05d4bfe55402af19b22b
Joshua Williams <joshuawilliams52006@gmail.com>
2023-05-31 10:08:03 -0700

    testing 3

--- playerProps.js
+++ playerProps.js
@@ -5,7 +5,60 @@ const resultDiv = document.getElementById('result');
 
 // Function to handle adding a new player props field
 function addPlayerPropsField() {
-  // ... code for adding a new player props field ...
+  const playerForm = document.getElementById('playerForm');
+
+  const playerPropsField = document.createElement('div');
+  playerPropsField.classList.add('playerPropsField');
+
+  const playerNameLabel = document.createElement('label');
+  playerNameLabel.textContent = 'Player Name:';
+  const playerNameInput = document.createElement('input');
+  playerNameInput.type = 'text';
+  playerNameInput.classList.add('playerName');
+  playerNameInput.placeholder = 'Enter player name';
+
+  const lineLabel = document.createElement('label');
+  lineLabel.textContent = 'Line:';
+  const lineInput = document.createElement('input');
+  lineInput.type = 'number';
+  lineInput.step = '0.5';
+  lineInput.classList.add('line');
+  lineInput.placeholder = 'Enter line';
+
+  const categoryLabel = document.createElement('label');
+  categoryLabel.textContent = 'Category:';
+  const categorySelect = document.createElement('select');
+  categorySelect.classList.add('category');
+  const optionPoints = document.createElement('option');
+  optionPoints.value = 'points';
+  optionPoints.textContent = 'Points';
+  const optionRebounds = document.createElement('option');
+  optionRebounds.value = 'rebounds';
+  optionRebounds.textContent = 'Rebounds';
+  const optionAssists = document.createElement('option');
+  optionAssists.value = 'assists';
+  optionAssists.textContent = 'Assists';
+
+  categorySelect.appendChild(optionPoints);
+  categorySelect.appendChild(optionRebounds);
+  categorySelect.appendChild(optionAssists);
+
+  playerPropsField.appendChild(playerNameLabel);
+  playerPropsField.appendChild(playerNameInput);
+  playerPropsField.appendChild(lineLabel);
+  playerPropsField.appendChild(lineInput);
+  playerPropsField.appendChild(categoryLabel);
+  playerPropsField.appendChild(categorySelect);
+
+  playerForm.insertBefore(playerPropsField, playerForm.lastElementChild);
+}
+
+// Function to simulate player performance
+function simulatePerformance(category) {
+  // ... code to simulate player performance based on category ...
+
+  // For demonstration purposes, generating a random performance value
+  return Math.random() * 10;
 }
 
 // Function to test player props line
